d41abe6b2f1a6b202c7d793628e7580e5807ed06
Sepehr Norouzi <s.norouzi@medrick.info>
2023-11-29 16:25:18 +0330

    Env added. store init. axios init. auth init.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
+  <h1>{{ message }}</h1>
 </template>
 
 <style>
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hi"
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,49 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userToken: null,
+    user: null,
+    userRefresh: null,
   },
   getters: {
+    getUserToken(state) {
+      return state.userToken;
+    },
+    getUserRefresh(state) {
+      return state.userRefresh;
+    },
+    getUser(state) {
+      return state.user;
+    },
   },
   mutations: {
+    setUserTokens(state, token) {
+      state.userToken = token.access,
+      state.userRefresh = token.refresh
+    },
+    setUser(state, user) {
+      state.user = user
+    }
   },
   actions: {
+    loginUser({ commit }, data) {
+      commit('setUserTokens', data.tokens)
+      commit('setUser', {
+        username: data.username
+      })
+    },
+
+    logoutUser({ commit }) {
+      commit('setUserTokens', {access: null, refresh: null})
+      commit('setUser', null)
+    }
   },
   modules: {
+
   }
 })
